078b86e26b88d16c99a38febd18516623308a181
Elisha Terada <elishaterada@gmail.com>
2016-10-15 15:14:22 -0700

    Configure default color theme based on Light Blue

--- src/app/app.js
+++ src/app/app.js
@@ -22,9 +22,35 @@ function config (
   $animateProvider.classNameFilter(/^(?:(?!ng-animate-disabled).)*$/)
 
   // Angular Material Setup
+  // Angular Material Setup
+  $mdThemingProvider.definePalette('customPalette', {
+    '50': 'E1F5FE',
+    '100': 'B3E5FC',
+    '200': '81D4FA',
+    '300': '4FC3F7',
+    '400': '29B6F6',
+    '500': '03A9F4',
+    '600': '039BE5',
+    '700': '0288D1',
+    '800': '0277BD',
+    '900': '01579B',
+    'A100': '80D8FF',
+    'A200': '40C4FF',
+    'A400': '00B0FF',
+    'A700': '0091EA',
+    'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                        // on this palette should be dark or light
+    'contrastDarkColors': ['50', '100', // hues which contrast should be 'dark' by default
+      '200', '300', '400', 'A100'],
+    'contrastLightColors': undefined    // could also specify this if default was 'dark'
+  })
+
   $mdThemingProvider.theme('custom')
-    .primaryPalette('deep-purple')
-    .accentPalette('blue')
+    .primaryPalette('customPalette')
+
+  $mdThemingProvider.theme('custom-dark')
+    .primaryPalette('customPalette')
+    .dark()
 
   $mdThemingProvider.setDefaultTheme('custom')
 
